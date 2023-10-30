5f7ca197a0360cc124fc736bccdc93e7b8569103
Alex Litvinov <aaalex.lit@gmail.com>
2023-10-30 19:23:38 +0100

    get execution environment from an environment variable

--- ingest/ml/ingest_ml.py
+++ ingest/ml/ingest_ml.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import os
 import sys
 import tempfile
 from datetime import datetime
@@ -33,7 +34,7 @@ SLACK_COLLECTION_NAME = 'mlzoomcamp_slack'
 
 
 @task(name="Index FAQ Google Document")
-def index_google_doc(local: bool):
+def index_google_doc():
     document_ids = ["1LpPanc33QJJ6BSsyxVg-pWNMplal84TdZtq10naIhD8"]
     logger.info('Loading google doc...')
     temp_creds = tempfile.NamedTemporaryFile()
@@ -45,11 +46,11 @@ def index_google_doc(local: bool):
     raw_docs = loader.load()
     temp_creds.close()
     add_to_index([Document.from_langchain_format(doc) for doc in raw_docs],
-                 collection_name=FAQ_COLLECTION_NAME, local=local)
+                 collection_name=FAQ_COLLECTION_NAME)
 
 
 @task(name="Index course schedule")
-def index_course_schedule(local: bool):
+def index_course_schedule():
     url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSkEwMv5OKwCdPfW6LgqQvKk48dZjPcFDrjDstBqZfq38UPadh0Nws1b57qOVYwzAjSufKnVf7umGWH/pubhtml'
     documents = TrafilaturaWebReader().load_data([url])
     for doc in documents:
@@ -57,25 +58,25 @@ def index_course_schedule(local: bool):
         doc.metadata['source'] = url
     add_to_index(documents,
                  collection_name=FAQ_COLLECTION_NAME,
-                 local=local,
                  overwrite=False
                  )
 
 
 @task(name="Index slack messages")
-def index_slack_messages(local: bool):
+def index_slack_messages():
     slack_reader = SlackReader(earliest_date=datetime(2022, 9, 1),
                                bot_user_id=BOT_USER_ID,
                                not_ignore_users=[AU_TOMATOR_USER_ID],
                                slack_token=Secret.load('slack-bot-token').get())
 
     documents = slack_reader.load_data(channel_ids=[ML_CHANNEL_ID])
-    add_to_index(documents, collection_name=SLACK_COLLECTION_NAME, local=local)
+    add_to_index(documents, collection_name=SLACK_COLLECTION_NAME)
 
 
-def add_to_index(documents, collection_name, overwrite=True, local=True):
+def add_to_index(documents, collection_name, overwrite=True):
     node_parser = SimpleNodeParser.from_defaults(chunk_size=384, chunk_overlap=20)
-    if local:
+    environment = os.getenv('EXECUTION_ENV', 'local')
+    if environment == 'local':
         milvus_vector_store = MilvusVectorStore(collection_name=collection_name,
                                                 dim=embedding_dimension,
                                                 overwrite=overwrite)
@@ -92,11 +93,11 @@ def add_to_index(documents, collection_name, overwrite=True, local=True):
 
 
 @flow(name="Update ML info Milvus index", log_prints=True)
-def fill_ml_index(local=False):
-    index_google_doc(local=local)
-    index_course_schedule(local=local)
-    index_slack_messages(local=local)
+def fill_ml_index():
+    index_google_doc()
+    index_course_schedule()
+    index_slack_messages()
 
 
 if __name__ == '__main__':
-    fill_ml_index(True)
+    fill_ml_index()

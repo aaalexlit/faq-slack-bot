120bccdcabdf573f7da5575a743172cffaf81a9b
Alex Litvinov <aaalex.lit@gmail.com>
2023-11-18 19:07:26 +0100

    ingest ML course github repos parallelize ingestion

--- ingest/ml/ingest_ml.py
+++ ingest/ml/ingest_ml.py
@@ -10,9 +10,9 @@ from typing import List
 from langchain.document_loaders import GoogleDriveLoader
 from langchain.embeddings import HuggingFaceEmbeddings
 from llama_index import StorageContext, VectorStoreIndex, ServiceContext
-from llama_index.readers import TrafilaturaWebReader
 from llama_index.node_parser import SimpleNodeParser
 from llama_index.readers import Document
+from llama_index.readers import TrafilaturaWebReader, GithubRepositoryReader
 from llama_index.vector_stores import MilvusVectorStore
 from prefect import flow, task
 from prefect.blocks.system import Secret
@@ -35,6 +35,53 @@ AU_TOMATOR_USER_ID = 'U01S08W6Z9T'
 ML_CHANNEL_ID = 'C0288NJ5XSA'
 FAQ_COLLECTION_NAME = 'mlzoomcamp_faq_git'
 SLACK_COLLECTION_NAME = 'mlzoomcamp_slack'
+
+
+def index_github_repo(owner: str,
+                      repo: str,
+                      branch: str,
+                      ignore_file_extensions: [str] = None,
+                      ignore_directories: [str] = None,
+                      ):
+    if ignore_file_extensions is None:
+        ignore_file_extensions = ['.jpg', '.png', '.gitignore', '.csv']
+    if ignore_directories is None:
+        ignore_directories = ['.github', '.gitignore', '2021', '2022', 'images']
+    documents = GithubRepositoryReader(
+        owner=owner,
+        repo=repo,
+        github_token=Secret.load('github-token').get(),
+        ignore_file_extensions=ignore_file_extensions,
+        ignore_directories=ignore_directories,
+    ).load_data(branch=branch)
+    for doc in documents:
+        doc.metadata['branch'] = branch
+        doc.metadata['owner'] = owner
+        doc.metadata['repo'] = repo
+    add_route_to_docs(documents, 'github')
+    add_to_index(documents, collection_name=FAQ_COLLECTION_NAME)
+
+
+@task(name="Index course github repo")
+def index_course_github_repo():
+    owner = 'DataTalksClub'
+    repo = 'machine-learning-zoomcamp'
+    branch = 'master'
+    index_github_repo(owner=owner,
+                      repo=repo,
+                      branch=branch)
+
+
+@task(name="Index book github repo")
+def index_book_github_repo():
+    owner = 'alexeygrigorev'
+    repo = 'mlbookcamp-code'
+    branch = 'master'
+    ignore_directories = ['.github', 'course-zoomcamp', 'images', 'util']
+    index_github_repo(owner=owner,
+                      repo=repo,
+                      branch=branch,
+                      ignore_directories=ignore_directories)
 
 
 @task(name="Index FAQ Google Document")
@@ -124,10 +171,12 @@ def add_to_index(documents: List[Document], collection_name: str, overwrite: boo
 @flow(name="Update ML info Milvus index", log_prints=True)
 def fill_ml_index():
     print(f"Execution environment is {os.getenv('EXECUTION_ENV', 'local')}")
+    index_slack_messages.submit()
     index_google_doc()
-    index_course_schedule()
-    index_evaluation_criteria()
-    index_slack_messages()
+    index_course_schedule.submit(wait_for=[index_google_doc])
+    index_evaluation_criteria.submit(wait_for=[index_google_doc])
+    index_course_github_repo.submit(wait_for=[index_google_doc])
+    index_book_github_repo.submit(wait_for=[index_google_doc])
 
 
 if __name__ == '__main__':
